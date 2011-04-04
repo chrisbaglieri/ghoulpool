70802071cd372f8462dbd209ca6ad2a525908fd9
Chris Baglieri <chris.baglieri@gmail.com>
2011-04-04 00:12:36 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'pg', '0.10.0'
+gem 'hassle', :git => 'git://github.com/koppen/hassle.git'
+gem 'simple_form'
+gem 'haml'
+gem 'authlogic'
+
+group :development, :test do
+  gem 'haml-rails'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'factory_girl'
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghoulpool::Application.config.secret_token = '5c494ce7a4211ace07554522d637b957dbc09884e7b05ab0226e14e6b7f990197c968adc3ad3be7f3ca6a077a8cd16f5657100427d52e8c75bbffa6234f533a9'
