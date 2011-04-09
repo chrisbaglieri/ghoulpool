e839a21436e77e208aae5a2a286d3808f7d4c7bd
Chris Baglieri <chris.baglieri@gmail.com>
2011-04-08 23:23:06 -0400

    finished testing model ar validations

--- spec/factories/ghoul_factory.rb
+++ spec/factories/ghoul_factory.rb
@@ -1,6 +1,6 @@
 Factory.define :ghoul do |ghoul|
-  ghoul.name 'Foo'
+  ghoul.name (0...8).map{65.+(rand(25)).chr}.join
   ghoul.born_on Date.new(2000, 12, 31)
   ghoul.died_on DateTime.now
-  ghoul.freebase_id  '/en/elizabeth_taylor'
+  ghoul.freebase_id  (0...8).map{65.+(rand(25)).chr}.join
 end
--- spec/models/ghoul_spec.rb
+++ spec/models/ghoul_spec.rb
@@ -8,4 +8,11 @@ describe Ghoul do
     end
   end
   
+  [:name, :freebase_id].each do |field|
+    it 'should validate uniqueness of #{field} on create' do
+      Factory(:ghoul, field => 'foo')
+      Factory.build(:ghoul, field => 'foo').save.should be_false
+    end
+  end
+  
 end
