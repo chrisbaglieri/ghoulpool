39426941a4284150e686ff9e93647ef2edbd1c52
Chris Baglieri <chris.baglieri@gmail.com>
2011-04-09 11:05:22 -0400

    added dashboard controller skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Ghoulpool::Application.routes.draw do
-  
-  resources :rules
-
-  resources :entries
-
-  resources :pools
 
   devise_for :users, :path_names => { :sign_up => "register", :sign_in => "login", :sign_out => "logout"  }
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    get "/logout" => "devise/sessions#destroy"
+  end
   
   root :to => "pages#home"
+  
+  match "dashboard" => 'dashboard#index', :via => :get
+  
+  resources :entries
   resources :ghouls
+  resources :pools
+  resources :rules
   
 end
