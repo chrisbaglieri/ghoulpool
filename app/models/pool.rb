0783176b1d61f3736117b7eca44c04d8169c12c1
Chris Baglieri <chris.baglieri@gmail.com>
2011-04-12 22:18:14 -0400

    added owned_by scope on pool and corresponding test

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -5,4 +5,6 @@ class Pool < ActiveRecord::Base
   has_one :rule
   belongs_to :owner, :class_name => "User", :foreign_key => :user_id
   has_and_belongs_to_many :users
+  
+  scope :owned_by, lambda { |owner| where(:user_id => owner) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,11 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should have_many(:entries) }
   it { should have_and_belong_to_many(:pools) }
+  
+  it "should find the pools they own" do
+    owner = Factory(:user)
+    Factory(:pool, :owner => owner)
+    pools = Pool.owned_by owner
+    pools.count.should == 1
+  end
 end
