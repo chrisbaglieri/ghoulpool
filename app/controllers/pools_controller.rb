a434d656e18c388f3340b80e0826dac96cae31af
Chris Baglieri <chris.baglieri@gmail.com>
2011-04-17 22:48:43 -0400

    stubbed out pools

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,2 +1,23 @@
 class PoolsController < ApplicationController
+  def index
+    @pools = current_user.pools
+  end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
 end
--- spec/controllers/pools_controller_spec.rb
+++ spec/controllers/pools_controller_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
 describe PoolsController do
+  before do
+    @user = Factory(:user)
+    sign_in @user
+  end
+  
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'show'" do
+    before do
+      @pool = Factory(:pool, :owner => @user)
+    end
+    
+    it "should be successful" do
+      get 'show', { :id => @pool.id }
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'edit'" do
+    before do
+      @pool = Factory(:pool, :owner => @user)
+    end
+    
+    it "should be successful" do
+      get 'edit', { :id => @pool.id }
+      response.should be_success
+    end
+  end
 
 end
