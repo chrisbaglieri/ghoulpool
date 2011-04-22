a67544ec02ee8f9c701667e824e38465c0cb2754
Chris Baglieri <chris.baglieri@gmail.com>
2011-04-21 22:50:47 -0400

    added alive?

--- app/models/ghoul.rb
+++ app/models/ghoul.rb
@@ -4,4 +4,15 @@ class Ghoul < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_uniqueness_of :freebase_id
   has_many :entries
+  
+  def alive?
+    return @alive if defined?(@alive)
+    @alive = true
+    if self.died_on.blank?
+      query = { "type" => "/people/deceased_person", "id" => "#{self.freebase_id}", "date_of_death" => [] }
+      matches = Ken.session.mqlread([query])
+      @alive = matches.blank?
+    end
+    @alive
+  end
 end
--- spec/models/ghoul_spec.rb
+++ spec/models/ghoul_spec.rb
@@ -11,4 +11,14 @@ describe Ghoul do
       Factory.build(:ghoul, field => 'foo').save.should be_false
     end
   end
+  
+  it 'should know someone is alive' do
+    ghoul = Factory(:ghoul, :freebase_id => '/en/charlie_sheen')
+    ghoul.alive?.should == true
+  end
+  
+  it 'should know someone is dead' do
+    ghoul = Factory(:ghoul, :freebase_id => '/en/elizabeth_taylor')
+    ghoul.alive?.should == true
+  end
 end
