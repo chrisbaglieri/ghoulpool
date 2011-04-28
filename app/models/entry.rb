a0cf984d619b71f4eeb7cbbafa7c45fca418abb0
Chris Baglieri <chris.baglieri@gmail.com>
2011-04-28 16:21:47 -0400

    added rules and potential value to entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,4 +3,11 @@ class Entry < ActiveRecord::Base
   belongs_to :ghoul
   belongs_to :pool
   has_one :rule, :through => :pool
+  
+  def value
+    return 0 if ghoul.died_on.blank?
+    days_alive = ghoul.died_on - ghoul.born_on
+    function = rule.function.gsub(':days_alive', days_alive.to_s)
+    instance_eval(function)
+  end
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -5,4 +5,15 @@ describe Entry do
   it { should belong_to(:ghoul) }
   it { should belong_to(:pool) }
   it { should have_one(:rule).through(:pool) }
+  
+  it 'should have a value of zero for a ghoul who is alive' do
+    ghoul = Factory(:ghoul, :died_on => nil)
+    entry = Factory(:entry, :ghoul => ghoul)
+    entry.value.should == 0
+  end
+  
+  it 'should have a value greater than zero for a ghoul who is dead' do
+    entry = Factory(:entry)
+    entry.value.should > 0
+  end
 end
