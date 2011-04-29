912bbd5b896d8fd5cf40895f83792ffea2a4a89c
Chris Baglieri <chris.baglieri@gmail.com>
2011-04-28 23:40:43 -0400

    finished testing entry controller

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -20,6 +20,18 @@ describe EntriesController do
       response.should render_template(:action => 'pools/show')
     end
     
+    it "should not create a new entry for a ghoul that's dead" do
+      post 'create', :pool_id => @pool.id, :freebase_id => "/en/elizabeth_taylor"
+      response.should render_template(:action => 'pools/new')
+    end
+  end
+  
+  describe "DELETE 'destroy'" do
+    it "should delete an entry" do
+      @entry = Factory(:entry, :owner => @user, :pool => @pool)
+      delete 'destroy', :pool_id => @pool.id, :id => @entry
+      response.should render_template(:action => 'pools/show')
+    end
   end
 
 end
