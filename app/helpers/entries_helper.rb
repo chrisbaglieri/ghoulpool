7d7b6b85cd25aa2efabe4c40b7dc7b33b8d60b93
Chris Baglieri <chris.baglieri@gmail.com>
2011-07-11 23:50:50 -0400

    cosmetic goodness

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -2,4 +2,14 @@ module EntriesHelper
   def freebase_url(freebase_id)
     "http://www.freebase.com/view#{freebase_id}"
   end
+  
+  def freebase_metadata(ghoul)
+    metadata = ghoul.name
+    if ghoul.born_on.blank?
+      metadata += " (birth date unknown)"
+    else
+      metadata += " (born #{ghoul.born_on.strftime('%m/%d/%Y')})"
+    end
+    metadata
+  end
 end
